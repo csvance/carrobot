65da5996e175883993378ecb764be6d7164f1bc0
Carroll Vance <vancecs@gmail.com>
2018-10-17 09:05:17 -0500

    Rename base node / remove uneeded ttransform / fix tf name

--- scripts/carrobot_node.py
+++ scripts/carrobot_node.py
@@ -6,26 +6,19 @@ from geometry_msgs.msg import PoseWithCovariance, TwistWithCovariance
 import tf
 
 
-class BaseNode(object):
+class CarRoBotNode(object):
     def __init__(self, integrate_num_samples=10):
         rospy.init_node('base')
         self._tf_broadcaster = tf.TransformBroadcaster()
 
         rospy.Subscriber("/odom", Odometry, queue_size=100)
 
-
-    # 100hz
     def _odom_callback(self, odom):
-
-        self._tf_broadcaster.sendTransform(odom.pose.position,
-                                           odom.pose.orientation, imu.header.stamp,
-                                           "base", "world")
-
         self._tf_broadcaster.sendTransform((0., 0., rospy.get_param('lidar_height')),
                                            (0., 0., 0., 0.),
                                            imu.header.stamp,
-                                           "neato_laser", "base")
+                                           "neato_laser", "base_link")
 
 if __name__ == '__main__':
-    pose = BaseNode()
+    pose = CarRoBotNode()
     rospy.spin()
