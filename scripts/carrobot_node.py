816c2c9bf96e1a8d6afe564b336983d86127c9ce
Carroll Vance <vancecs@gmail.com>
2018-11-07 19:25:26 -0600

    Adding proportional RPM control of LIDAR

--- scripts/carrobot_node.py
+++ scripts/carrobot_node.py
@@ -4,6 +4,7 @@ import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from std_msgs.msg import UInt16
 import tf
 import time
 import pigpio
@@ -14,10 +15,23 @@ class CarRoBotNode(object):
         self._tf_broadcaster = tf.TransformBroadcaster()
 
         rospy.Subscriber("/odom", Odometry, self._odom_callback, queue_size=100)
+        rospy.Subscriber("/rpms", UInt16, self._rpm_callback, queue_size=5)
         self.vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
 
         self._pi = pigpio.pi()
-        self._pi.hardware_PWM(12, 100, 0.28 * 1000000)
+        self._pi.hardware_PWM(12, 100, 0.3 * 1000000)
+
+    def _rpm_callback(self, rpm):
+
+        proportion = 220.0 / max(1.0, float(rpm.data))
+        drive = max(min(1.0, proportion), 0.6)
+
+        print("RPM: %f DRIVE: %f P: %f" % (rpm.data, drive, proportion))
+        self._pi.hardware_PWM(12, 100, 0.3 * 1000000)
+        # self._pi.hardware_PWM(12, 100, drive * 1000000)
+
+
+
 
     def _odom_callback(self, odom):
         self._tf_broadcaster.sendTransform((rospy.get_param("~base_lidar_x"),
